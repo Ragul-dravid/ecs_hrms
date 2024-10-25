53151a954f1db1849dddab35b8a65c0e7d325cae
Ragul-dravid <dhurai737@gmail.com>
2024-10-25 14:34:43 +0530

    update url

--- src/config/URL.js
+++ src/config/URL.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL : "http://13.213.208.92:7080/ecshr/api"
-  // baseURL : "https://hrisasia.com/ecshr/api"
+  baseURL : "http://13.213.208.92:7080/ecshrms/api"
+  // baseURL : "https://hrisasia.com/ecshrms/api"
 });
 
 // Add a request interceptor
